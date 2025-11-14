69040e813801ddbe4a333b7072ead66cf8ff3bcc
sdarwin <samuel.d.darwin@gmail.com>
2025-11-14 05:48:26 -0700

    remove resetModules: true,

--- .config/jest.js
+++ .config/jest.js
@@ -20,7 +20,6 @@ export default function commonConfig(outputName) {
       ],
     ],
     testTimeout: 60000,
-    workerIdleMemoryLimit: '100MB',
-    resetModules: true,
+    workerIdleMemoryLimit: '200MB',
   }
 }
